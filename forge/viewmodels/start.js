06a9d4777b5c4ab722216009d93bac3e2543fda8
bimManager <kirillkozlov@ymail.com>
2019-06-24 16:47:02 +0300

    working on authentication

--- forge/viewmodels/start.js
+++ forge/viewmodels/start.js
@@ -17,8 +17,8 @@ if (config.credentials.client_id == null
 let app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json({ limit: "50mb" }));
-app.use("/api/forge/oauth", require("./route/oauth"));
-app.use("/api/forge/oss", require("./route/oss"));
+app.use("/api/forge/oauth", require("./routes/oauth"));
+app.use("/api/forge/oss", require("./routes/oss"));
 app.use("/api/forge/modelderivative", require("./route/modelderivative"));
 app.use((err, req, res, next) =>
     {
