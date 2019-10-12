ed4887c28bd4ac197edef0fb5c84d492e5d475ef
bimManager <kirillkozlov@ymail.com>
2019-10-12 15:44:48 +0100

    almost done with hw4

--- meadowlark/site/public/FontChooser.js
+++ meadowlark/site/public/FontChooser.js
@@ -3,16 +3,21 @@ class FontChooser extends React.Component {
 	super(props);
 	this.formRef = React.createRef();
 	this.checkboxRef = React.createRef();
-	this.textRef = React.createRef();
+	localStorage.setItem("initialFontSize", this.props.size);
 	console.log(this.props.bold);
-	this.state = { fontWeight: (this.props.bold == "true" ? "bold" : "normal"), size: parseInt(this.props.size, 10) };
+	this.state = { fontColor: "black",
+	    fontWeight: this.props.bold == "true" ? "bold" : "normal",
+		       fontSize: parseInt(this.props.size, 10) };
     }
     toggleFormElements() {
 	const formRef = this.formRef.current;
+	const checkboxRef = this.checkboxRef.current;
 	if (formRef.hidden)
 	    formRef.hidden = false;
 	else
 	    formRef.hidden = true;
+	if (this.state.fontWeight = "bold")
+	    checkboxRef.checked = true;
     }
     
     toggleBoldness() {
@@ -25,11 +30,35 @@ class FontChooser extends React.Component {
     }
     
     incrementFontSize() {
-	this.setState({size: this.state.size + 1 });
+	if (this.state.fontSize == this.props.max) {
+	    return ;
+	} else if (this.state.fontSize == this.props.max - 1) {
+	    this.setState({fontColor: "red"});
+	} else if (this.state.fontColor == "red") {
+	    this.setState({fontColor: "black"});
+	}
+	this.setState({fontSize: this.state.fontSize + 1 });
+    }
+    componentDidUpdate() {
+	console.log(this.state.fontSize);
     }
     
     decrementFontSize() {
-	this.setState({size: this.state.size - 1 });
+	if (this.state.fontSize == this.props.min) {
+	    return ;
+	} else if (this.state.fontSize - 1 == this.props.min) {
+	    this.setState({fontColor: "red"});
+	}
+	else if (this.state.fontColor == "red") {
+	    this.setState({fontColor: "black"});
+	}
+	this.setState({fontSize: this.state.fontSize - 1 });
+    }
+
+    restoreDefaultValues() {
+	const initialFontSize = parseInt(localStorage.getItem("initialFontSize"));
+	this.setState({ fontSize: initialFontSize });
+	this.setState({ fontColor: "black" });
     }
 
     render() {
@@ -38,12 +67,15 @@ class FontChooser extends React.Component {
 		<span ref={this.formRef} hidden={true}>
 		<input type="checkbox" id="boldCheckbox" ref={this.checkboxRef} onClick={this.toggleBoldness.bind(this)}/>
 		<button id="decreaseButton" onClick={this.decrementFontSize.bind(this)}>-</button>
-		<span id="sizeSpan">{this.state.size}</span>
+		<span id="sizeSpan">{this.state.fontSize}</span>
 		<button id="increaseButton" onClick={this.incrementFontSize.bind(this)}>+</button>
 		</span>
 		<span id="textSpan" ref={this.textRef}
-	    style={{fontSize: this.state.size, fontWeight: this.state.fontWeight}}
-	    onClick={this.toggleFormElements.bind(this)}>{this.props.text}</span>
+	    style={{color: this.state.fontColor, fontSize: this.state.fontSize, fontWeight: this.state.fontWeight}}
+	    onClick={this.toggleFormElements.bind(this)}
+	    onDoubleClick={this.restoreDefaultValues.bind(this)}>
+		{this.props.text}
+	    </span>
 	       </div>
 	);
     }
