0497239a7ef39c38405f8d94f7627ff7b2797b63
BimManager <kirillkozlov@ymail.com>
2020-04-05 18:19:10 -0700

    added 404 and 500 middlewares

--- nodejs/template/app.js
+++ nodejs/template/app.js
@@ -17,9 +17,11 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 
+const config = require('./config');
+
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', config.port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -27,12 +29,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /* send a static html */
 app.get('/', (req, res) => {
-    res.sendfile('index.html');
+    res.sendFile('index.html');
 });
 
 /* render a jade file */
 app.get('/jade', (req, res) => {
     res.render('index');
+});
+
+app.get('/error', (req, res, next) => {
+	next(new Error());
+});
+
+app.use((req, res, next) => {
+	res.status(404)
+		.type('text/html')
+		.send('<h1>404 Not Found</h1>');
+});
+
+app.use((err, req, res, next) => {
+	res.status(500)
+		.type('text/html')
+		.send('<h1>500 Server Error</h1>');
 });
 
 http
---
+++ nodejs/template/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	port: process.env.PORT || 3000,
+	credentials: {
+		cookieSecret: 'randomly generated string'
+	}
+};
